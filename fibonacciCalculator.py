a5b1d0225c2522f2016a555e5227b193edbd6f31
mstar27 <39783498+mstar27@users.noreply.github.com>
2018-11-30 00:44:52 -0800

    Add files via upload

--- fibonacciCalculator.py
+++ fibonacciCalculator.py
@@ -1,4 +1,4 @@
-#######################################     this program is a basic fibonacci number sequence calculator           ###########################################
+#######################################   this program is a basic fibonacci number sequence calculator      ###########################################
 
 #just a basic functionality test, variable declaration, test print
 if 1==1:
@@ -12,7 +12,7 @@ varX = 0
 varY = 1
 varZ = varX + varY
 
-#swap variables for moving from one iteration to next in fibonacci;vars: A, B
+#swap variables for moving from one iter to next in fibonacci;vars: A, B
 varA = 0
 varB = 0
 """variable declaration ends here"""
@@ -20,69 +20,83 @@ varB = 0
 
 """user interface starts here"""
 #introduction to what the code does
-print ("this program will out put various numbers in the fibonacci sequence based on the number of iterations the user desires")
+print ("this program will out put various numbers in the fibonacci sequence based on the number of iters the user desires")
 
-#prompt to get user input for iterations(converted to integer)
-userInput = int(input('how many iterations do you want?'))
-print("calculating for", userInput, "iterations")
+#prompt to get user input for iters(converted to integer)
+userIn_Iters = int(input("how many iters do you want?"))
+print("calculating for", userIn, "iters")
+
+#make this a function that also checks if valid input. if invalid will give error message and repeat. if good will set variable
+def func_UserIn_ShowIter():
+	userIn_ShowIter = input.lower("If you would like to see all iterations calculated type, 'y' or 'yes'. If not, type 'n' or 'N' ")
+
+	if userIn_ShowIter is 1:
+		b_ShowIter = 1
+		print("Showing all iterations calculated")
+
+	elif userIn_ShowIter is 0:
+		b_ShowIter = 0
+		print("Only showing the final iteration calculated")
+
+func_UserIn_ShowIter
 """user interface ends here"""
 
 
 """main body of the code begins here"""
-#the count for how many iterations the function has run
+#the count for how many iters the function has run
 #should I start this at 1 for the while loop, or shoul I keep 0 and add one at start of while loop?
-mathIterationCount = 0
+iterCount_Math = 0
 #used only for the while loop that is used once
-firstIterationCount = 0
+iterCount_First = 0
 
-#need this for first iteration
-while firstIterationCount == 0:
-	#may take this out depending on starting value. need to do something so first iteration isn't 0
-	mathIterationCount += 1
+#need this for first iter
+while iterCount_First == 0:
+	#may take this out depending on starting value. need to do something so first iter isn't 0
+	iterCount_Math += 1
 
-	#prints the starting values declared at start of module. the first 3 fibonacci iteration numbers; raises function iteration count
-	print(varX, "Iter#", mathIterationCount)
+	#prints the starting values declared at start of module. the first 3 fibonacci iter numbers; raises function iter count
+	print(varX, "Iter#", iterCount_Math)
 
-	mathIterationCount += 1
-	print(varY, "Iter#", mathIterationCount)
+	iterCount_Math += 1
+	print(varY, "Iter#", iterCount_Math)
 
 	#could have the counter increase after the variable value changing or leave before it
-	mathIterationCount += 1
+	iterCount_Math += 1
 	varZ = varX + varY
-	print(varZ, "Iter#", mathIterationCount)
+	print(varZ, "Iter#", iterCount_Math)
 
 	#move the relevant variables into swap variables
 	varA = varY
 	varB = varZ
 
 	#add to the counter, ending this one time use while loop 
-	firstIterationCount += 1
+	iterCount_First += 1
 
 
 #function to be run number of times based on input from user gained start of program
-#first way i structured the loop running the function(  for g in range (1, userInput):  ))
-#alternate way to do this loop(  while mathIterationCount < userInput:  )
-while mathIterationCount <= userInput:
-#taking last two iteration ends and pluging back into equation
+#first way i structured the loop running the function(  for g in range (1, userIn_Iters):  ))
+#alternate way to do this loop(  while iterCount_Math < userIn_Iters:  )
+while iterCount_Math <= userIn_Iters:
+#taking last two iter ends and pluging back into equation
 	varX = varA
 	varY = varB
 
-	#get the answer for this iteration
+	#get the answer for this iter
 	varZ = varX + varY
 
-	#prints iteration value, kinda the point of the code XD
-	print(varZ, "Iter#", mathIterationCount)
+	#prints iter value, kinda the point of the code XD
+	print(varZ, "Iter#", iterCount_Math)
 
 	#move the relevant variables into swap variables
 	varA = varY
 	varB = varZ
 
-	#adds to iteration counter
-	mathIterationCount += 1
+	#adds to iter counter
+	iterCount_Math += 1
 
 
 """start of: code to print a gramaticly nice final value phrase"""
-#print("the value of the", mathIterationCount, )
+#print("the value of the", iterCount_Math, )
 
 """end of: code that makes the program print a gramaticly nice final value phrase"""
 
@@ -91,7 +105,7 @@ while mathIterationCount <= userInput:
 
 
 """end output and program end begins here"""
-#without the -1, the below print command is outputting a number one higher than the actual number of iterations
-print("the number of iterations calculated were:", mathIterationCount-1)
+#without the -1, the below print command is outputting a number one higher than the actual number of iters
+print("the number of iters calculated were:", iterCount_Math-1)
 print("done")
 """program has run and is complete"""
